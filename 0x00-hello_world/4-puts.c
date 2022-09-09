d5275c18101acafbfc87b5223ddde6a54dad375a
MandisaAF <mandisamtambo2@gmail.com>
2022-09-08 23:52:48 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0
+ * Return: 0 after printing the function
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
-
 }
